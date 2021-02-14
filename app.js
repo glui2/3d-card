dc9221ba670bc9d6e677e1d13af595a5bbb614fc
Gavin <gavin.lui@gmail.com>
2021-02-14 18:47:52 +1100

    perspective animation

---
+++ app.js
@@ -0,0 +1,34 @@
+// movement animation
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+
+// items
+const title = document.querySelector(".title");
+const sneaker = document.querySelector(".sneaker img");
+const purchase = document.querySelector(".purchase button");
+const description = document.querySelector(".info h3");
+const sizes = document.querySelector(".sizes");
+
+// moving animation event
+container.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 30; // pageX pageY gives x & y position of the cursor on the card
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 30;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
+// reset card when cursor leaves container
+container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+
+  //pop in
+  title.style.transform = "translateZ(0px)";
+});
+
+// revert animation transition to none when mouse enters container
+container.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+
+  // pop out
+  title.style.transform = "translateZ(150px)";
+});
